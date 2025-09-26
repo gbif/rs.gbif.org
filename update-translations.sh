8426c9ba4e4ab7c6cce3bc4b5f505af141835859
Matthew Blissett <matt@blissett.me.uk>
2025-09-26 11:21:57 +0200

    Avoid null changes from translations.

--- update-translations.sh
+++ update-translations.sh
@@ -15,7 +15,7 @@ fi
 # If an extension file has only the "issued" date changed, there's no need to release that.
 # Undo the change in that case.
 for i in $(git status --short | grep '^ M ' | awk '{print $2}'); do
-    if [[ -z "$(git diff --numstat --ignore-matching-lines='^  dc:issued' $i)" ]]; then
+    if [[ -z "$(git diff --numstat --ignore-matching-lines='^ +dc:issued' $i)" ]]; then
         echo "No change in translations for $i"
         git checkout --quiet $i
     else
